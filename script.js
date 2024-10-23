f4a1e0a6800c3c92a5438c58238e9d5dd7530e70
unknown <abigail_rick@mailfence.com>
2024-10-23 13:23:38 -0500

    renamed memeInput id
    renamed id for readability

---
+++ script.js
@@ -0,0 +1,4 @@
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+const generatorBtn = document.getElementById('generatorBtn');
+const memeHolder = document.getElementById('memeHolder');
